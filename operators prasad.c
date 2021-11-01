91a2053ad69e2d1bec3fb253f4724adf0254c0da
prasadteegireddy <93113389+prasadteegireddy@users.noreply.github.com>
2021-11-01 10:59:06 +0530

    Add files via upload

---
+++ operators prasad.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	int a=15,b=25,c,d,e,f,g,h;
+	c=a+b;
+	d=a-b;
+	e=a*b;
+	f=a/b;
+	g=a%b;
+	h=c+d+e+f+g;
+	printf("sum of %d and %d is %d",a,b,c);
+	printf("\n diffrence between %d is %d is %d",a,b,d);
+	printf("\n product of %d is %d is %d",a,b,e);
+	printf("\n quotient when %d is divided %d id %d",a,b,f);
+	printf("\n remainder when %d is divided %d is %d",a,b,g);
+	printf("\nsum of all arthematic operatrs is %d",h);
+
+}
