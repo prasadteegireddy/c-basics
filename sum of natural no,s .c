dfcc71fd3ae8a020e45732d8baff16867210360a
prasadteegireddy <93113389+prasadteegireddy@users.noreply.github.com>
2021-11-02 10:20:02 +0530

    Add files via upload

---
+++ sum of natural no,s .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+int n,a,b,c;
+n=10;
+a=n*(n+1)/2;
+b=n*(n+1);
+c=n*n;
+printf("sum of first %d natural numbers is %d",n,a);
+printf("\n sum of first %d even natural numers is %d",n,b);
+printf("\n sum of first %d odd natural numers is %d",n,c);
+}
